d009f3134f6c4d1db532a7c237ba381e25bc1a49
Caliber357 <hack3rmon@gmail.com>
2020-11-17 13:08:12 +0100

    test

--- November Projekt/Program.cs
+++ November Projekt/Program.cs
@@ -6,7 +6,7 @@ namespace November_Projekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("reee");
             Console.ReadLine();
         }
     }
