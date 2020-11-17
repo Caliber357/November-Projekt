4384d7e70ca77e19d7304819b2d1a52dc4f9feaf
Caliber357 <hack3rmon@gmail.com>
2020-11-17 13:05:36 +0100

    Start

---
+++ November Projekt/Dealer.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace November_Projekt
+{
+    public class Dealer
+    {
+        
+    }
+}
---
+++ November Projekt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace November_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
