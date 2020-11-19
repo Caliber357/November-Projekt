987a800ae5a33fb82a73f3514eeb2e7663c59bc6
Caliber357 <hack3rmon@gmail.com>
2020-11-19 16:30:54 +0100

    list stuff

--- November Projekt/Card.cs
+++ November Projekt/Card.cs
@@ -7,34 +7,30 @@ namespace November_Projekt
     public class Card
     {
         private Random rng = new Random();
-        private int cardValue = 0;
         private bool lost = false;
-        //private string[] card = {"Ace", "2"}
 
+        public Card(int n)
+        {
+            name = n;
 
-        public void whatCard(){
-            int v = rng.Next(1,11);
-            cardValue = v;
-            if (cardValue == 1 || cardValue == 11){
-                System.Console.WriteLine("Do you your ace to be worth 1 or 11");
-                string answer = Console.ReadLine();
-                switch(answer){
-                    case "1":
-                    cardValue = 1;
-                    break;
-                    case "11":
-                    cardValue = 11;
-                    break;
-                    default:
-                    System.Console.WriteLine("That was not a valid answer");
-                    break;
-                }
-            }
-            System.Console.WriteLine("The value of your card is");
         }
-        public bool isOver21(){
+        private void WhatCard(){
+            switch(name){
+                case 1:
+
+                break;
+            }
             
-            
+        }
+        private int name = 0;
+        public int GetName(){
+            return name;
+        }
+
+        public bool isOver21()
+        {
+
+
             return lost;
         }
 
--- November Projekt/Program.cs
+++ November Projekt/Program.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 namespace November_Projekt
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+        List<Card> Cards = new List<Card>();
+            for (int i = 1; i < 53; i++)
+            {
+                Cards.Add(new Card(i));
+            }
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                System.Console.WriteLine(Cards[i].GetName());
+            }
             Console.ReadLine();
         }
     }
